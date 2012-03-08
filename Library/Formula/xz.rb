66f8d131f5cc0746d738ecde2288439866379fdb
Nicholas Devenish <n.e.devenish@sussex.ac.uk>
2012-03-08 23:02:23 +0000

    Xz works

--- Library/Formula/xz.rb
+++ Library/Formula/xz.rb
@@ -4,7 +4,8 @@ class Xz < Formula
   homepage 'http://tukaani.org/xz/'
   url 'http://tukaani.org/xz/xz-5.0.3.tar.bz2'
   sha256 '5a11b9e17bfcda62319c5a8c4a2062dc81607a316d3f6adff89422d81ec1eae9'
-
+  platforms :mac, :linux
+  
   def options
     [["--universal", "Build a universal binary."]]
   end
