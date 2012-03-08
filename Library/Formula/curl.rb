592ea48585f1f5fbbdc365743805de85d644dc94
Nicholas Devenish <n.e.devenish@sussex.ac.uk>
2012-03-08 23:05:15 +0000

    Allow linux-curl installs as a binary

--- Library/Formula/curl.rb
+++ Library/Formula/curl.rb
@@ -4,9 +4,10 @@ class Curl < Formula
   homepage 'http://curl.haxx.se/'
   url 'http://curl.haxx.se/download/curl-7.24.0.tar.bz2'
   sha256 'ebdb111088ff8b0e05b1d1b075e9f1608285e8105cc51e21caacf33d01812c16'
+  platforms :mac, :linux
 
   keg_only :provided_by_osx,
-            "The libcurl provided by Leopard is too old for CouchDB to use."
+            "The libcurl provided by Leopard is too old for CouchDB to use." if SystemCommand.platform == :mac
 
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
